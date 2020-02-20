31cdae812b7ff12cc3065a5c07e21b35a72f8a5c
Garrett Watson <garrettbauer@gmail.com>
2020-02-21 00:07:04 +0900

    Created styled components Grid layout with Header, Menu, and CardsWrapper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import Card from './components/Card';
+import styled from 'styled-components';
+
+const Wrapper = styled('div')`
+  display: grid;
+  grid-template-columns: 5vw 1fr 5vw;
+  grid-template-rows: repeat(3, auto);
+  grid-column-gap: 3vmin;
+`;
+
+const GridHeader = styled('div')`
+  grid-column: 1 / span 3;
+`;
+
+const GridMenu = styled('div')`
+  grid-column: 1 / span 3;
+`;
+
+const CardsWrapper = styled('div')`
+  grid-column: 2 / span 1;
+`;
+
+const H1 = styled('h1')`
+  color: #444444;
+`;
+
+/*
+Notes:
+Card size: 273 x 339px
+Image size: ~250px
+Padding/inner margin: 11px
+Description text: 9px
+Price text: 8px
+Stars: 7px
+Margin above description: 10px
+grid-gap: 20px
+*/
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Wrapper>
+      <GridHeader>
+        <H1>Flower Shop</H1>
+      </GridHeader>
+      <GridMenu>
+        <h2>Add Flower</h2>
+      </GridMenu>
+      <CardsWrapper>
+        <Card />
+      </CardsWrapper>
+    </Wrapper>
   );
 }
 

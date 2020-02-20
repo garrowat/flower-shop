d6516ee65c850ac8aba6dfc2acdd9e08d0c9a494
Garrett Watson <garrettbauer@gmail.com>
2020-02-21 00:15:25 +0900

    Remove unneeded css files due to move to styled components

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CardWrapper = styled('div')`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default () => (
+  <div>
+    Product
+  </div>
+);

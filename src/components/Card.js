63c23e1214b15ec486bac3ef9a6313b26996fd22
Garrett Watson <garrettbauer@gmail.com>
2020-02-21 13:51:22 +0900

    Add css, state, and handlers for mouse hovering

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const CardWrapper = styled('div')`
@@ -6,10 +6,15 @@ const CardWrapper = styled('div')`
   flex-direction: column;
   align-items: center;
   flex: 0 0 auto;
-  backgound: #fff;
+  background: #fff;
   box-shadow: 0 6px 20px rgba(0,0,0,0.09), 0 5px 5px rgba(0,0,0,0.06);
   padding: 11px;
   margin-bottom: 25px;
+
+  &:hover {
+    filter: opacity(50%);
+    transition: all 0.1s ease-out;
+  }
 `;
 
 const CardImage = styled('img')`
@@ -21,6 +26,7 @@ const CardImage = styled('img')`
 const CardName = styled('span')`
   font-size: 3vmin;;
   font-weight: bold;
+  margin-bottom: 5px;
 `;
 
 const CardPrice = styled('span')`
@@ -37,21 +43,38 @@ const InactiveStar = styled(`span`)`
   opacity: 0.5;
 `;
 
-export default ({ image, name, price, stars }) => (
-  <CardWrapper>
-    <CardImage alt="A Blue Flower" src={`/images/${image}-flower.png`} />
-    <CardName>{name}</CardName>
-    <CardPrice>${(Math.round(price * 100) / 100).toFixed(2)}</CardPrice>
-    <Stars>
-      {
-        [1, 2, 3, 4, 5].map((star) => {
-          if (star <= stars) {
-            return <span>★</span>
-          } else {
-            return <InactiveStar>★</InactiveStar>
-          }
-        })
-      }
-    </Stars>
-  </CardWrapper>
-);
+export default ({ image, name, price, stars }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = (e) => {
+    setIsHovered(true);
+    console.log({isHovered})
+  };
+
+  const handleMouseOut = (e) => {
+    setIsHovered(false);
+    console.log({isHovered})
+  };
+
+  return (
+    <CardWrapper
+      onMouseEnter={handleMouseEnter}
+      onMouseOut={handleMouseOut}
+    >
+      <CardImage alt="A Blue Flower" src={`/images/${image}-flower.png`} />
+      <CardName>{name}</CardName>
+      <CardPrice>${(Math.round(price * 100) / 100).toFixed(2)}</CardPrice>
+      <Stars>
+        {
+          [1, 2, 3, 4, 5].map((star) => {
+            if (star <= stars) {
+              return <span>★</span>
+            } else {
+              return <InactiveStar>★</InactiveStar>
+            }
+          })
+        }
+      </Stars>
+    </CardWrapper>
+  );
+};

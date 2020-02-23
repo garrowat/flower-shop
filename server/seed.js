0def22a4ab3579fa4c452ff0be1599ed34628910
Garrett Watson <garrettbauer@gmail.com>
2020-02-23 23:58:02 +0900

    Create seed data for use on server boot-up

---
+++ server/seed.js
@@ -0,0 +1,7 @@
+const seedData = [
+  { image: 'blue', name: 'Blue Flower', price: 80, stars: 4 },
+  { image: 'orange', name: 'Orange Flower', price: 17.60, stars: 3 },
+  { image: 'pink', name: 'Pink Flower', price: 40, stars: 5 },
+];
+
+module.exports = seedData;

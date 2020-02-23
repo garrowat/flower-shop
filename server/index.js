abf511d446ea15d5664bd745beb5003876b64737
Garrett Watson <garrettbauer@gmail.com>
2020-02-23 22:41:39 +0900

    Add actual inventory routes, sync with postgres via sequelize (drop the table on launch)

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const { sequelize, Inventory } = require('./models');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-app.get('/', () => res.send('Heyooooo'));
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Flower shop is listening on port ${port}`);
+app.post('/inventory', (req, res) => {
+  const data = req.body;
+  res.status(201).send({data});
+});
+
+app.get('/inventory', (req, res) => res.send('Heyooooo'));
+
+sequelize.sync({ force: true }).then(async () => {
+  app.listen(port, () => {
+    console.log(`Flower shop is running on port ${port}`);
+  });
 });
